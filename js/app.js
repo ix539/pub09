3368c9030ece7bdec7df5782e567fc7e0918301d
ix539 <kfvideo01@gmail.com>
2022-05-19 17:33:43 -0700

    We wrote our first program.. boom

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log('Hello!')
+//var
+//let
+//const
+
+
+
+function basicHello() {
+    //function delclartion 
+    console.log("Hello2");
+};
+
+// const basicHello2 = function() {
+//     //function express
+//     console.log("Hello3");
+// };
+
+const basicMath = function(num1, num2) {
+    console.log(Math.min(num1,num2))
+};
+
+const basicMultiplication = function(a, b, c) {
+    console.log(a*b*c);
+    //here is the symbol for multiplication *
+}
+
+
+
+basicHello();
+// basicHello2();
+
+console.log('Remaining code');
+
+basicMath(45, 1000);
+basicMultiplication(5,2,3);
+
+
+
