097dc1b28d9d5e374fbd23931d0fd5f7b7473966
ix539 <kfvideo01@gmail.com>
2022-05-19 18:02:59 -0700

    binding a function to multiply four parameters

--- js/app.js
+++ js/app.js
@@ -19,11 +19,15 @@ const basicMath = function(num1, num2) {
 };
 
 const basicMultiplication = function(a, b, c) {
-    console.log(a*b*c);
+    console.log(Math.floor(a + b + c));
     //here is the symbol for multiplication *
 }
 
-
+const multiplyFour = function(a, b, c, d) {
+    //binding a function to multiply four paramaters
+    console.log(Math.floor(a * b * c * d));
+    //using mathFloor function to execute command 
+}; 
 
 basicHello();
 // basicHello2();
@@ -31,7 +35,7 @@ basicHello();
 console.log('Remaining code');
 
 basicMath(45, 1000);
-basicMultiplication(5,2,3);
-
+basicMultiplication(5,2,8);
+multiplyFour(45,1,1,1);    
 
 
